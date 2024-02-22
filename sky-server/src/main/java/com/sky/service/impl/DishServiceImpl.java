591ab807e5e31fecc49b51c75bd38b34bc01f818
ikaros <a193932849@163.com>
2024-02-22 23:01:38 +0800

    新增菜品业务开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,8 +1,18 @@
 package com.sky.service.impl;
 
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author Ikaros
@@ -10,5 +20,35 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    @Transactional
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+        log.info("向菜品表和口味表同时插入数据");
+        //向菜品表插入一条数据
+        Dish d = new Dish();
+        BeanUtils.copyProperties(dishDTO, d);
+        //此处在插入时，mybatis-plus会自动帮忙设置主键id
+        dishMapper.insert(d);
+        log.info("dish_id: {}", d.getId());
+        Long dishId = d.getId();
 
+        //向口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+
+        if (flavors != null && flavors.size() > 0){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+    }
 }
